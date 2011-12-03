4144da4695b0afdfe82aaac8294ce560e5e88c3f
Maarten Claes <maartencls@gmail.com>
2011-12-03 19:59:35 +0100

    generateRandomBlock() en formatting

--- src/tetris/Sidebar.java
+++ src/tetris/Sidebar.java
@@ -3,15 +3,16 @@ package tetris;
 import java.awt.*;
 import javax.swing.*;
 
-
 public class Sidebar extends JPanel {
 
-    JPanel sidePanel;
-    static String block = "Lblock";
-    static int lines;
-    static int score;
+  JPanel sidePanel;
+  static String block = "Lblock";
+  static int lines;
+  static int score;
 
-    public Sidebar(){
+  protected JLabel blockLabel;
+
+  public Sidebar() {
     System.out.println("new panel created, new block is " + block);
     sidePanel = new JPanel();
     sidePanel.setBackground(Color.gray);
@@ -23,49 +24,30 @@ public class Sidebar extends JPanel {
     NextBlock.setFont(new Font("sansserif", Font.BOLD, 15));
     sidePanel.add(NextBlock);
 
-    ImageIcon iconI = new ImageIcon("C:\\Users\\vdb\\Documents\\NetBeansProjects\\Tetris\\src\\tetris\\Iblock.png");
-    ImageIcon iconJ = new ImageIcon("C:\\Users\\vdb\\Documents\\NetBeansProjects\\Tetris\\src\\tetris\\Jblock.png");
-    ImageIcon iconL = new ImageIcon("C:\\Users\\vdb\\Documents\\NetBeansProjects\\Tetris\\src\\tetris\\Lblock.png");
-    ImageIcon iconO = new ImageIcon("C:\\Users\\vdb\\Documents\\NetBeansProjects\\Tetris\\src\\tetris\\Oblock.png");
-    ImageIcon iconS = new ImageIcon("C:\\Users\\vdb\\Documents\\NetBeansProjects\\Tetris\\src\\tetris\\Sblock.png");
-    ImageIcon iconT = new ImageIcon("C:\\Users\\vdb\\Documents\\NetBeansProjects\\Tetris\\src\\tetris\\Tblock.png");
-    ImageIcon iconZ = new ImageIcon("C:\\Users\\vdb\\Documents\\NetBeansProjects\\Tetris\\src\\tetris\\Zblock.png");
+    // Generate random block
+    JLabel blockLabel = new JLabel();
+    this.generateRandomBlock();
+    sidePanel.add(blockLabel);
 
-    JLabel label = new JLabel();
-
-    // Icoon voor next block instellen
-    if (Grid.randomNum == 0){
-        label.setIcon(iconI);
-    }else if(Grid.randomNum == 1){
-        label.setIcon(iconJ);
-    }else if(Grid.randomNum == 2){
-        label.setIcon(iconL);
-    }else if(Grid.randomNum == 3){
-        label.setIcon(iconO);
-    }else if(Grid.randomNum == 4){
-        label.setIcon(iconS);
-    }else if(Grid.randomNum == 5){
-        label.setIcon(iconT);
-    }else if(Grid.randomNum == 6){
-        label.setIcon(iconZ);
-    }
-    sidePanel.add(label);
-
+    // Start Button
     JButton startButton = new JButton("Start game");
     startButton.setFont(new Font("sansserif", Font.BOLD, 15));
     startButton.setFocusable(false);
     sidePanel.add(startButton);
 
+    // Pause Button
     JButton pauseButton = new JButton("Pause game");
     pauseButton.setFont(new Font("sansserif", Font.BOLD, 15));
     pauseButton.setFocusable(false);
     sidePanel.add(pauseButton);
 
+    // Stop
     JButton stopButton = new JButton("Stop game");
     stopButton.setFont(new Font("sansserif", Font.BOLD, 15));
     stopButton.setFocusable(false);
     sidePanel.add(stopButton);
 
+    // Score
     JLabel Score = new JLabel("Score: " + score);
     Score.setFont(new Font("sansserif", Font.BOLD, 18));
     sidePanel.add(Score);
@@ -78,10 +60,48 @@ public class Sidebar extends JPanel {
     highscores.setFont(new Font("sansserif", Font.BOLD, 15));
     highscores.setFocusable(false);
     sidePanel.add(highscores);
+  }
+
+  public void setLines(int line) {
+    lines = line;
+
+  }
+
+  /**
+   * Een willekeurige nieuwe blok genereren
+   */
+  public void generateRandomBlock() {
+    // Icoon voor next block instellen
+    ImageIcon icon;
+    String imagePath = "C:\\Users\\vdb\\Documents\\NetBeansProjects\\Tetris\\src\\tetris\\";
+    switch (Grid.randomNum) {
+      case 1:
+        icon = new ImageIcon(imagePath + "Iblock.png");
+        break;
+      case 2:
+        icon = new ImageIcon(imagePath + "Jblock.png");
+        break;
+      case 3:
+        icon = new ImageIcon(imagePath + "Lblock.png");
+        break;
+      case 4:
+        icon = new ImageIcon(imagePath + "Oblock.png");
+        break;
+      case 5:
+        icon = new ImageIcon(imagePath + "Sblock.png");
+        break;
+      case 6:
+        icon = new ImageIcon(imagePath + "Tblock.png");
+        break;
+      case 7:
+        icon = new ImageIcon(imagePath + "Zblock.png");
+        break;
+      default:
+        // Should not get here
+        return;
     }
 
-    public void setLines(int line) {
-        lines = line;
-        
-    }
+    blockLabel.setIcon(icon);
+  }
+
 }
--- src/tetris/TetrisUI.java
+++ src/tetris/TetrisUI.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package tetris;
 
 import javax.swing.JOptionPane.*;
@@ -11,60 +10,55 @@ import java.awt.Frame.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+
 /**
  *
  * @author vdb
  */
 public class TetrisUI {
 
-    public static JFrame gameUI;
-    public static Sidebar tetrisSidebar;
-    public static PanelCreator panelCreator;
+  public static JFrame gameUI;
+  public static Sidebar tetrisSidebar;
+  public static PanelCreator panelCreator;
 
-        public void GUI() {
-                gameUI = new JFrame("Java Tetris Game!");
+  public void GUI() {
+    gameUI = new JFrame("Java Tetris Game!");
 
-                gameUI.addWindowListener(new WindowAdapter() {
-                        public void windowClosing(WindowEvent e) {
-                                System.exit(0);
-                        }
-                });
-                panelCreator = new PanelCreator();
-                Grid tetrisGrid;
+    gameUI.addWindowListener(new WindowAdapter() {
+
+      public void windowClosing(WindowEvent e) {
+        System.exit(0);
+      }
+    });
+    panelCreator = new PanelCreator();
+    Grid tetrisGrid;
 //                Sidebar tetrisSidebar;
-                Menu tetrisMenu;
+    Menu tetrisMenu;
 
 
 
-                gameUI.setLocation(100,100);
-                gameUI.setSize(446,603);
-                gameUI.setTitle("Java Tetris Game!!!");
-                gameUI.setVisible(true);
+    gameUI.setLocation(100, 100);
+    gameUI.setSize(446, 603);
+    gameUI.setTitle("Java Tetris Game!!!");
+    gameUI.setVisible(true);
 
-                tetrisGrid = panelCreator.createGrid(gameUI);
-                tetrisSidebar = panelCreator.createSidebar(gameUI);
-                tetrisMenu = panelCreator.createMenu(gameUI);
-                
-                gameUI.addKeyListener(tetrisGrid);
+    tetrisGrid = panelCreator.createGrid(gameUI);
+    tetrisSidebar = panelCreator.createSidebar(gameUI);
+    tetrisMenu = panelCreator.createMenu(gameUI);
+
+    gameUI.addKeyListener(tetrisGrid);
 
 
 //                gameUI.pack();
-            }
+  }
 
+  public static void main(String[] args) {
+    TetrisUI tetrisUI = new TetrisUI();
+    tetrisUI.start();
+  }
 
-
-    public static void main(String[] args) {
-        TetrisUI tetrisUI = new TetrisUI();
-        tetrisUI.start();
-    }
-
-    public void start(){
-        SwingUtilities.invokeLater(new Runnable() {
-           public void run() {
-               System.out.println("hallo");
-               TetrisUI.this.GUI();
-           }
-        });
-    }
+  public void start() {
+    System.out.println("hallo");
+    TetrisUI.this.GUI();
+  }
 }
-
